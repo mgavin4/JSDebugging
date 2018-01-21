4665d9695c80896f310238e7d0d5e8c8915d52d2
Matt <matthewcgavin@gmail.com>
2018-01-20 20:58:32 -0500

    added cache busting

---
+++ buildScripts/generateMockData.js
@@ -0,0 +1,21 @@
+import jsf from 'json-schema-faker';
+import {schema} from './mockDataSchema';
+import fs from 'fs';
+import chalk from 'chalk';
+import faker from "faker"
+
+jsf.extend("faker", function() {
+  return faker
+})
+
+const json = JSON.stringify(jsf(schema));
+
+
+fs.writeFile("./src/api/db.json", json, function(err) {
+  if (err) {
+    return console.log(chalk.red(err)); // eslint-disable-line no-console
+  }
+  else {
+    console.log(chalk.green("Mock data generated.")); // eslint-disable-line no-console
+  }
+});
--- src/index.test.js
+++ src/index.test.js
@@ -9,11 +9,11 @@ describe('Our first test', ()=>{
 });
 
 describe('index.html', ()=>{
-  it('should say hello', (done)=>{ // add done parameter for asynchronous testing
+  it('should have h1 that says Users', (done)=>{ // add done parameter for asynchronous testing
     const index = fs.readFileSync('./src/index.html',"utf-8");
     jsdom.env(index, function(err, window){
       const h1 = window.document.getElementsByTagName('h1')[0];
-      expect(h1.innerHTML).to.equal("Hello world!");
+      expect(h1.innerHTML).to.equal("Users");
       done(); // and done here
       window.close();
     });
